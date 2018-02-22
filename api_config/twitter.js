14c9037a16f2eceee4df560b0879fbff99905f6f
AYush <getayush96@gmail.com>
2018-02-22 18:51:46 +0530

    auth change
    test

--- api_config/twitter.js
+++ api_config/twitter.js
@@ -1,6 +1,6 @@
 var Twit = require('twit');
 var t = new Twit({
-	consumer_key:         'aTMVoUes845sPb62PXkAVf9yA',
+	consumer_key:         'MVoUes845sPb62PXkAVf9yA',
 	consumer_secret:      'MM8adawzqjyxNBW8bMRQTYumL95nrPnR7X7g0h1xUOHw1UMOKz',
 	access_token:         '256006992-ekFn2F6kbF4ZqUsk1G6L6AHT41HfIApW8KXlmF3R',
 	access_token_secret:  'kOCDeMaFu8rqCD0PTmcRdnMGLvU4CRS4fkSiDrqD30Nfz',
